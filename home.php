2efa7a2ab17fc8f1aaf78c61836a392a999d7607
Mark Dave Catubig <119982799+catubi123@users.noreply.github.com>
2025-02-09 12:52:01 +0800

    Update home.php

--- home.php
+++ home.php
@@ -11,7 +11,22 @@
     <link rel="stylesheet" href="w3.css">
 </head>
 <body>
-    <h1>WELCOME USER</h1>
-    <button><a href="index.php">Logout</a></button>
+    <!-- Navbar -->
+<div class="w3-bar w3-cyan">
+    <span class="w3-bar-item w3-large">Dashboard</span>
+    <a href="index.php" class="w3-bar-item w3-button w3-right w3-red w3-round-xlarge">Log out</a>
+    <a href="#" class="w3-bar-item w3-button w3-right">nav</a>
+    <a href="#" class="w3-bar-item w3-button w3-right">nav</a>
+    <a href="#" class="w3-bar-item w3-button w3-right">nav</a>
+    <a href="#" class="w3-bar-item w3-button w3-right">Home</a>
+
+</div>
+<div class="w3-container w3-padding-32">
+    <h1 class="w3-center">Welcome to the Dashboard</h1>
+    <p class="w3-center">This is a secure area. This Page is in development.</p>
+    <p class="w3-center">Coming Soon......</p>
+
+
+</div>
 </body>
-</html>
+</html>'
